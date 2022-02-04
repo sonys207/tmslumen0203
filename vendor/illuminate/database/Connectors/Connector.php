394ac984750a6c588726b6817e803dde66139dcb
Jichen Liu <jichen.liu85@gmail.com>
2022-02-04 18:33:52 -0500

    ssl

--- vendor/illuminate/database/Connectors/Connector.php
+++ vendor/illuminate/database/Connectors/Connector.php
@@ -66,6 +66,7 @@ class Connector
         if (class_exists(PDOConnection::class) && ! $this->isPersistentConnection($options)) {
             return new PDOConnection($dsn, $username, $password, $options);
         }
+	
         return new PDO($dsn, $username, $password, $options);
     }
 
